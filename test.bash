46ffd3c77dbaa3da60425f708007e9efad4f2dfe
Yuma Yamada <j3371vkf@sit.ac.jp>
2025-12-03 13:50:56 +0900

    add github actions

---
+++ test.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Yamada Yuma
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+        echo NG at Line $1
+        ret = 1
+}
+
+res = 0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "${res}" = 0 ] && echo OK
+exit $res
